e334597063d81b5e8abdc50683db3ff4c8ba79f0
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-03-12 09:16:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App() {
+
+  // const myHooksState = useState({
+  //   ciudad: 'Madrid',
+  //   escuela: 'Wild Code School',
+  //   infected: false
+  // })
+
+  // const myState = myHooksState[0];
+  // const changeMyState = myHooksState[1];
+
+  //useState nos devuelve un array con dos items. El primer item será nuestro estado, y el segundo será una función que nos permite cambiar este estado si la llamamos.
+
+    //MI PRIMER ESTADO
+    const [myState, changeMyState] = useState({
+      ciudad: 'Madrid',
+      escuela: 'Wild Code School',
+      infected: false
+    })
+
+    //MI SEGUNDO ESTADO
+    const [myName, changeMyName] = useState('Jaime')
+
+    //MI TERCER ESTADO
+    const [myApiCall, changeMyApiCall] = useState('')
+
+    //useEffect se activa despues del primer renderizado y se volverá a activar cuando se detecte un cambio de valor en el item que hemos introducido en el array del segudno argumento.
+    useEffect(()=>{
+      console.log('HOLA!!!')
+    }, [myState.ciudad])
+
+    //Equivalente a componentDidMount()
+    useEffect(()=>{
+      fetch('https://yesno.wtf/api')
+      .then(res=>res.json())
+      .then(resJSON=>changeMyApiCall(resJSON))
+    }, [])
+
+    return (
+      <div className="App">
+        <h1>Hola {myState.escuela} desde {myState.ciudad} Te saluda {myName} :D</h1>
+        <button onClick={()=> changeMyState((prevState) =>({...prevState, ciudad: 'Barcelona'}))}>Change city</button>
+        <button onClick={()=>changeMyName('Escuadra')}>CHANGE MY NAME</button>
+      </div>
+    );    
+
 }
 
 export default App;
+
